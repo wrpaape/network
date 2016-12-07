2a538aeedc6c69f929d92d83913805f5967cac97
Reid Paape <wrpaape@gmail.com>
2016-12-07 15:33:10 -0600

    trying to fix debug statement for send_response

--- src/server/sendmail_server.c
+++ src/server/sendmail_server.c
@@ -312,7 +312,7 @@ send_response(const int connect_descriptor,
 			 "\r\nConnection: close\r\n\r\n",
 			 23);
 
-	DEBUG("send_response:\n\"\"\"%s\n\"\"\"\n",
+	DEBUG("send_response:\n\"\"\"\n%s\n\"\"\"\n",
 	      &buffer[0]);
 
 	return write_report(connect_descriptor,
